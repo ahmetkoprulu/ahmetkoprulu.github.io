63a70a776384761f22f8fa3854d9952f73307fe5
Ahmet Ensar Köprülü <31247645+ahmetkoprulu@users.noreply.github.com>
2019-02-05 17:57:33 +0300

    planner

---
+++ precache-manifest.657393e91970446c46ab49bd6fe8f56c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b4e09195fc8268e57061",
+    "url": "/static/js/main.b4e09195.chunk.js"
+  },
+  {
+    "revision": "11b5d3b02a8451f8d858",
+    "url": "/static/js/1.11b5d3b0.chunk.js"
+  },
+  {
+    "revision": "b4e09195fc8268e57061",
+    "url": "/static/css/main.072e4e45.chunk.css"
+  },
+  {
+    "revision": "11b5d3b02a8451f8d858",
+    "url": "/static/css/1.a2abd2c2.chunk.css"
+  },
+  {
+    "revision": "2869b743a47331a28878b7f01b21d69b",
+    "url": "/index.html"
+  }
+];
